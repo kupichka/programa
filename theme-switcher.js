29d04e696ba839e613565f0897946777f33ddbbf
kupichka <spongebobphotoscan@gmail.com>
2025-02-15 12:23:07 +0200

    New theme
    A new theme! - White mode
    An enhancement to the theme switcher, now removes the previously loaded theme file.
    Fixes to 9b-term 2

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,9 +1,24 @@
 document.getElementById('style-switcher').addEventListener('change', function() {
     var selectedCss = this.value;
+    var existingLink = document.getElementById('dynamic-theme');
+
+    if(existingLink) {
+        existingLink.remove();
+    }
 
     var newCssLink = document.createElement('link');
+    newCssLink.id = 'dynamic-theme';
     newCssLink.rel = 'stylesheet';
     newCssLink.href = selectedCss;
 
     document.head.appendChild(newCssLink);
+});
+
+const themes = ['theme 1-1.css', 'theme 1-3.css', 'theme 3-2.css', 'theme 4-2.css', 'theme 5-1.css', 'theme white.css'];
+themes.forEach(theme => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.as = 'style';
+    link.href = theme;
+    document.head.appendChild(link);
 });
